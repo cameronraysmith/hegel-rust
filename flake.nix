dd770eef5ca3c9ed064eba0e1a49a7bf466ef147
Liam DeVoe <orionldevoe@gmail.com>
2026-01-29 16:27:49 -0500

    check in cargo.lock

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Hegel Rust SDK - Property-based testing SDK for Hegel";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hegel.url = "git+ssh://git@github.com/antithesishq/hegel";
+  };
+
+  outputs = { nixpkgs, hegel, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "hegel";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        # hegel binary on PATH so build.rs finds it
+        nativeBuildInputs = [
+          hegel.packages.${system}.default
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.cargo
+          pkgs.rustc
+          hegel.packages.${system}.default
+        ];
+      };
+    };
+}
